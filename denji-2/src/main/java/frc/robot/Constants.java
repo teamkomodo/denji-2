3b7d98ceac01b1e0caff0ebd748d7c06ab5a864e
Realtent <realtentwo@gmail.com>
2023-09-23 10:30:48 -0600

    Intake Subsystem copied over

--- denji-2/src/main/java/frc/robot/Constants.java
+++ denji-2/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.RobotBase;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +19,25 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+  //Controls
+  public static final int XBOX_CONTROLLER_PORT = 0;
+  public static final double XBOX_JOYSTICK_THRESHOLD = 0.05D;
+  public static final int JOYSTICK_PORT = 1;// HF Joystick
+  public static final int BUTTONS_PORT = 2; // A-PAC Player 1
+  public static final int SELECTOR_PORT = 3; // A-PAC Player 2
+
+//Intake
+  public static final int INTAKE_MOTOR_ID = ;
+  public static final int INTAKE_THRESHOLD_CURRENT = ; // Amps
+  
+  //Claw
+  public static final int CLAW_SOLENOID_FORWARD_CHANNEL = ;
+  public static final int CLAW_SOLENOID_REVERSE_CHANNEL = ;
+  public static final int TOF_SENSOR_ID = ;
+  public static final double TOF_OFFSET_MM = ; // Distance = read() + TOF_OFFSET_MM
+
+//Joint
+  public static final int JOINT_MOTOR_ID = ;
+  public static final int JOINT_ZERO_SWITCH_CHANNEL = ;
+  public static final double JOINT_SLOW_MODE_MULTIPLIER = ;
 }
