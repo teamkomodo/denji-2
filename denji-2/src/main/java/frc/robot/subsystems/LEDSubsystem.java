2420e01af8fde846a2959dd4d4c688fb46a0fd7c
Realtent <realtentwo@gmail.com>
2023-10-07 11:17:58 -0600

    Removed cone pattens

--- denji-2/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ denji-2/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -9,7 +9,6 @@ import static frc.robot.Constants.*;
 public class LEDSubsystem extends SubsystemBase {
 
     public static final double IDLE_PATTERN = 0.43; //Sparkle, Color 1 on Color 2
-    public static final double CONE_SIGNAL_PATTERN = 0.65; //Solid Color: Orange
     public static final double CUBE_SIGNAL_PATTERN = 0.91; //Solid Color: Violet
 
     private Spark controller = new Spark(LED_STRIP_PWM_CHANNEL);
@@ -29,10 +28,6 @@ public class LEDSubsystem extends SubsystemBase {
     public Command setSolidColor(int colorId) {
         // Not including black
         return this.runOnce(() -> setPattern(0.57 + (0.02 * (colorId % 21))));
-    }
-
-    public Command coneSignalCommand() {
-        return setPatternCommand(CONE_SIGNAL_PATTERN);
     }
 
     public Command cubeSignalCommand() {
